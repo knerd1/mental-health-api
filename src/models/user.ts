a96ca7f12d37fce8bfa228c4c0d4063d75307e17
Ashutosh <ap9696446@gmail.com>
2025-04-10 21:32:00 +0530

    Added Dummy Data

---
+++ src/models/user.ts
@@ -0,0 +1,29 @@
+export enum UserRole {
+	Therapist = "therapist",
+	Client = "client",
+}
+
+export interface User {
+	id: string;
+	email: string;
+	hashed_password: string;
+	role: UserRole;
+}
+
+// Added only for testing purposes
+export const USERS: User[] = [
+	{
+		email: "yourMail@gmail.com",
+		id: "7z6ydcm",
+		role: UserRole.Client,
+		hashed_password:
+			"$2b$10$.MftzcPPsR5TUTYRyWGyQu9H9Fd3Q6olBlccI1qIAY3qXH7OQ.bQO", // dummy "123456"
+	},
+	{
+		email: "myName@gmail.com",
+		id: "41m3lxk",
+		role: UserRole.Therapist,
+		hashed_password:
+			"$2b$10$.MftzcPPsR5TUTYRyWGyQu9H9Fd3Q6olBlccI1qIAY3qXH7OQ.bQO", // dummy "123456"
+	},
+];
