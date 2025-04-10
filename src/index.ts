ef142fba55af9b4010481c369691c383d69125c6
Ashutosh <ap9696446@gmail.com>
2025-04-10 21:32:19 +0530

    Add CORS support and refactor authentication routes

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import authRoutes from './routes/auth';
+import consultationRoutes from './routes/consultations';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use('/auth', authRoutes);
+app.use('/consultations', consultationRoutes);
+
+const { PORT } = process.env;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
